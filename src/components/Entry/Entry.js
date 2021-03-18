908c4135a2ef9f3553d676a82c12632a873aeed6
Nazar <wolfter12@gmail.com>
2021-03-18 13:52:22 +0200

    Added current status

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -20,7 +20,10 @@ function Entry({ entry: { id, name, statusHistory, priority } }) {
       <Card.Header as="h5">{name}</Card.Header>
       <Card.Body>
         <Card.Subtitle className="mb-2 text-muted" key="status">
-          Status:
+          Current status: {statusHistory[0].status}
+        </Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted" key="status">
+          Status history:
           <ListGroup variant="flush">{statusHistoryList}</ListGroup>
         </Card.Subtitle>
         <Card.Subtitle className="mb-2 text-muted" key="priority">
