d846551c1fff50e4d2e66b9b7a3b60471819a25b
Nazar <wolfter12@gmail.com>
2021-03-18 13:22:33 +0200

    Updated App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import AddEntry from '../AddEntry';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import GroceryList from '../GroceryList/GroceryList';
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <Container>
+      <h1>Grocery List</h1>
+      <AddEntry />
+      <GroceryList />
+    </Container>
+  );
 }
 
 export default App;
