0b79dd62fb79f24757be6cd2e4984d0eda47e20d
Nazar <wolfter12@gmail.com>
2021-03-18 13:37:28 +0200

    Added filter

--- src/components/GroceryList/GroceryList.js
+++ src/components/GroceryList/GroceryList.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Entry from '../Entry';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { RAN_OUT, HAVE, ALL } from '../../configs/constants';
 
 function GroceryList() {
+  const [filterParam, setFilterParam] = useState(ALL);
+
   const list = useSelector((state) => state.list);
-  const listItems = list.map((entry) => <Entry key={entry.id} entry={entry} />);
-  return <div>{listItems}</div>;
+
+  const listItems = list
+    .filter(({ statusHistory }) => {
+      if (filterParam === ALL) {
+        return true;
+      }
+      return statusHistory[0].status === filterParam;
+    })
+    .map((entry) => <Entry key={entry.id} entry={entry} />);
+
+  const onFilterSelect = (e) => {
+    console.log(e);
+    setFilterParam(e);
+  };
+  return (
+    <div>
+      <DropdownButton
+        variant="outline-secondary"
+        title="Filter"
+        id="input-group-dropdown-filter"
+        onSelect={onFilterSelect}
+        className="mb-2"
+      >
+        <Dropdown.Item eventKey={RAN_OUT}>{RAN_OUT}</Dropdown.Item>
+        <Dropdown.Item eventKey={HAVE}>{HAVE}</Dropdown.Item>
+        <Dropdown.Item eventKey={ALL}>{ALL}</Dropdown.Item>
+      </DropdownButton>
+      {listItems}
+    </div>
+  );
 }
 
 export default GroceryList;
